954c30169cf2e305dcee6c74f4d2a74969e020a0
marcio-azevedo <marcio.azevedo.83@gmail.com>
2018-07-30 13:55:40 +0100

    Update output folder.

--- build.fsx
+++ build.fsx
@@ -14,7 +14,7 @@ let gitHome = "https://github.com/" + gitOwner
 // The name of the project on GitHub
 let gitProjectName = "the-three-ways-presentation"
 // The name of the GitHub repo subdirectory to publish slides to
-let gitSubDir = "default"
+let gitSubDir = "output"
 
 open FsReveal
 open Fake
